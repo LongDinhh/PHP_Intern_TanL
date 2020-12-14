e7632e5304ec50596a0a9145b7e902cd5e98a49b
Tan Le <lelechannel9922@gmail.com>
2020-12-14 15:22:42 +0700

    bai2_submit

--- bai2/bai2.php
+++ bai2/bai2.php
@@ -40,46 +40,11 @@ function show_prime($a, $b){
             echo $i, '<br>';
     }
 }
-//function isPrimeNumber(){
-//    // so nguyen n < 2 khong phai la so nguyen to
-////    if ($n < 2) {
-////        return false;
-////    }
-//    // check so nguyen to khi n >= 2
-//    $squareRoot = sqrt (30 );
-//    for($i = 11; $i <= $squareRoot; $i ++) {
-//        if (30 % $i == 0) {
-//            return [
-//                'i'=>''
-//            ];
-//        }
-//
-//    }
-//    return [
-//        'i'=>$i
-//    ];
-//
-//}
-//    function check(){
-//        for($i=11;$i<=30;$i++){
-//            echo $i;
-//            if(30%$i==0){
-//                return [
-//                    'number'=>0,
-//                    'message'=>''
-//                ];
-//            }
-//            return [
-//              'number'=>1,
-//               'i'=>$i
-//            ];
-//        }
-//    }
 ?>
 <form action="" method="post">
     <input type="text" name="number" value="">
     <input type="submit" name="submit" value="Ket qua">
 </form>
 <?php
-main();
+    main();
 ?>
