de524c3da606df8a0aa3b6c7f08fc31aa1bbb38c
Tan Le <lelechannel9922@gmail.com>
2020-12-14 14:27:08 +0700

    submit

--- bai2/bai2.php
+++ bai2/bai2.php
@@ -1,9 +1,51 @@
 <?php
-    if (isset($_POST['submit'])){
-        $number = $_POST['number'];
+    function validate($number){
+
+        if ($number != '11-30'){
+            return[
+                'code'=>0,
+                'message'=>'Khong dung dinh dang'
+            ];
+        }
+        return[
+            'code'=>1,
+            'message'=>''
+        ];
+    }
+    function main(){
+        if (isset($_POST['submit'])){
+            $number = $_POST['number'];
+            $numberValidate = validate($number);
+            $checkValidate = check();
+            if($numberValidate['code']!==1){
+                echo $numberValidate['message'];
+            }else{
+                if ($checkValidate['number']==1){
+                    echo $checkValidate['i'];
+                }
+            }
+
+        }
+    }
+    function check(){
+        for($i=11;$i<=30;$i++){
+            if(30%$i==0){
+                return [
+                    'number'=>0,
+                    'message'=>''
+                ];
+            }
+            return [
+              'number'=>1,
+               'i'=>$i
+            ];
+        }
     }
 ?>
 <form action="" method="post">
     <input type="text" name="number" value="">
     <input type="submit" name="submit" value="Ket qua">
 </form>
+<?php
+main();
+?>
