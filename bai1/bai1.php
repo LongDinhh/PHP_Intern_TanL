0ae67e246ab7797cf8de51ea3221e1df8085823c
Tan Le <lelechannel9922@gmail.com>
2020-12-14 15:18:10 +0700

    submit_4

--- bai1/bai1.php
+++ bai1/bai1.php
@@ -2,11 +2,22 @@
 
 function validate($a, $b, $c)
 {
-    if(!is_numeric($a) && !is_numeric($b) && !is_numeric($c)){
-//        $error = "Phai la so";
+    if (!is_numeric($a) || $a === 0 || empty($a)) {
         return [
             'code' => 0,
-            'message' => 'Phai la so'
+            'message' => 'a phai la so khac khong va khong duoc trong'
+        ];
+    }
+    if (!is_numeric($b) || empty($b)) {
+        return [
+            'code' => 0,
+            'message' => 'b phai la so va khong duoc de trong'
+        ];
+    }
+    if (!is_numeric($c) || empty($c)) {
+        return [
+            'code' => 0,
+            'message' => 'c phai la so va khong duoc de trong'
         ];
     }
     return [
@@ -17,64 +28,71 @@ function validate($a, $b, $c)
 
 function main()
 {
-    if (isset($_POST['submit'])){
+    if (isset($_POST['submit'])) {
 
         $a = $_POST['number_a'];
         $b = $_POST['number_b'];
         $c = $_POST['number_c'];
+
+        //
         $statusValidate = validate($a, $b, $c);
-        $deltaValidate = ptb($a,$b,$c);
+
+
+        $deltaValidate = ptb($a, $b, $c);
+
         if ($statusValidate['code'] !== 1) {
             echo $statusValidate['message'];
-
-        } else {
-            if($deltaValidate['number']==0){
-                echo $deltaValidate['message'];
-            }else if($deltaValidate['number']==1){
-                echo $deltaValidate['message'].'<br>'.$deltaValidate['x'];
-            }else if($deltaValidate['number']==2){
-                echo $deltaValidate['message'].'<br>'.$deltaValidate['x1'].'<br>'.$deltaValidate['x2'];
-            }
-//            echo $deltaValidate['result'];
+            return;
         }
+        if ($deltaValidate['number'] === 0) {
+            echo $deltaValidate['message'];
+            return;
+        }
+        if ($deltaValidate['number'] === 1) {
+            echo $deltaValidate['message'] . '<br>' . 'x1 = ' . $deltaValidate['x1'] . '<br>' . 'x2 = ' . $deltaValidate['x2'];
+            return;
+        }
+        if ($deltaValidate['number'] === 2) {
+            echo $deltaValidate['message'] . '<br>' . 'x1 = ' . $deltaValidate['x1'] . '<br>' . 'x2 = ' . $deltaValidate['x2'];
+            return;
+        }
+
     }
 }
 
-function ptb($a,$b,$c)
+function ptb($a, $b, $c)
 {
-    if ($a==0){
-        return false;
-    }
     $delta = ($b * $b) - (4 * $a * $c);
-    $x = -$b /(2*$a);
-    $x1 = (-$b - sqrt($delta))/2*$a;
-    $x2 = (-$b+sqrt($delta))/2*$a;
-    if ($delta<0){
+    if ($delta < 0) {
         return [
-            'number'=>0,
-            'message'=>'Phuong trinh vo nghiem'
+            'number' => 0,
+            'message' => 'Phuong trinh vo nghiem',
+            'x1' => null,
+            'x2' => null
         ];
-//        $result =  "Phuong trinh vo nghiem";
-    }else if($delta==0){
-        return[
-            'number'=>1,
-            'x'=>$x,
-            'message'=>'Phuong trinh co nghiem kep'
+    }
+    $x = -$b / (2 * $a);
+    if ($delta === 0) {
+        return [
+            'number' => 1,
+            'message' => 'Phuong trinh co nghiem kep',
+            'x1' => $x,
+            'x2' => $x
         ];
-
-    }else{
-        return[
-            'number'=>2,
-            'message'=>'Phuong trinh co 2 nghiem',
-            'x1'=>$x1,
-            'x2'=>$x2
+    }
+    $x1 = (-$b - sqrt($delta)) / 2 * $a;
+    $x2 = (-$b + sqrt($delta)) / 2 * $a;
+    if ($delta > 0) {
+        return [
+            'number' => 2,
+            'message' => 'Phuong trinh co 2 nghiem',
+            'x1' => $x1,
+            'x2' => $x2
         ];
-//        $result = "Phuong trinh co hai nghiem <br> x1 = $x1 <br> x2 = $x2";
     }
 }
 
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -111,12 +129,10 @@ function ptb($a,$b,$c)
                 <input type="text" name="number_c" value="">
             </td>
         </tr>
-            <input type="submit" value="Ket qua" name="submit">
+        <input type="submit" value="Ket qua" name="submit">
     </table>
 </form>
 <?php
-//    echo "$a.X^2+$b.X+$c==0.<br>";
-//    echo $result;
 main();
 ?>
 </body>
