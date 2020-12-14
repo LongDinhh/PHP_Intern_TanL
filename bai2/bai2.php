f8dffd83a13f522e4871073042a68624617b1b0b
Tan Le <lelechannel9922@gmail.com>
2020-12-14 16:17:38 +0700

    bai2_submit-1

--- bai2/bai2.php
+++ bai2/bai2.php
@@ -1,50 +1,63 @@
 <?php
-    function validate($number){
-
-        if ($number != '11-30'){
-            return[
-                'code'=>0,
-                'message'=>'Khong dung dinh dang'
-            ];
-        }
-        return[
-            'code'=>1,
-            'message'=>''
+function validate($number)
+{
+    $array = (explode('-', $number));
+    if ($number != $array['0'] . '-' . $array['1']) {
+        return [
+            'code' => 0,
+            'message' => 'Khong dung dinh dang'
         ];
     }
-    function main(){
-        if (isset($_POST['submit'])){
-            $number = $_POST['number'];
-            $numberValidate = validate($number);
-            $checkValidate = show_prime(11,30);
-            if($numberValidate['code']!==1){
-                echo $numberValidate['message'];
-            }else{
-                echo $checkValidate;
-            }
+    return [
+        'code' => 1,
+        'message' => ''
+    ];
+}
 
+function main()
+{
+    if (isset($_POST['submit'])) {
+        $number = $_POST['number'];
+//            echo $number.'<br>';
+//            $array = (explode('-',$number));
+//            echo '<br>';
+//            echo $array['0'];
+        $numberValidate = validate($number);
+        $checkValidate = showPrime($number);
+        if ($numberValidate['code'] !== 1) {
+            echo $numberValidate['message'];
+            return;
         }
+        echo $checkValidate;
     }
-function check_prime($n)
+}
+
+function checkPrime($n)
 {
     for ($i = 2; $i <= sqrt($n); $i++) {
-        if ($n % $i == 0){
+        if ($n % $i == 0) {
             return false;
         }
     }
     return true;
 }
-function show_prime($a, $b){
-    for ($i = $a; $i <= $b; $i++){
-        if(check_prime($i))
+
+function showPrime($number)
+{
+    $array = (explode('-', $number));
+    $a = $array['0'];
+    $b = $array['1'];
+    for ($i = $a; $i <= $b; $i++) {
+        if (checkPrime($i))
             echo $i, '<br>';
     }
 }
+
 ?>
 <form action="" method="post">
     <input type="text" name="number" value="">
     <input type="submit" name="submit" value="Ket qua">
 </form>
 <?php
-    main();
+main();
 ?>
