2c036fd63fe48586c6e370bfaec0ca6d1d85ea6b
Tan Le <lelechannel9922@gmail.com>
2020-12-14 14:50:35 +0700

    submit_1

--- bai2/bai2.php
+++ bai2/bai2.php
@@ -16,34 +16,50 @@
         if (isset($_POST['submit'])){
             $number = $_POST['number'];
             $numberValidate = validate($number);
-            $checkValidate = isPrimeNumber();
+            $checkValidate = show_prime(11,30);
             if($numberValidate['code']!==1){
                 echo $numberValidate['message'];
             }else{
-                echo $checkValidate['i'];
+                echo $checkValidate;
             }
 
         }
     }
-function isPrimeNumber(){
-    // so nguyen n < 2 khong phai la so nguyen to
-//    if ($n < 2) {
-//        return false;
-//    }
-    // check so nguyen to khi n >= 2
-    $squareRoot = sqrt (30 );
-    for($i = 2; $i <= $squareRoot; $i ++) {
-        if (30 % $i == 0) {
-            return [
-                'i'=>''
-            ];
+function check_prime($n)
+{
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($n % $i == 0){
+            return false;
         }
-        return [
-            'i'=>$i
-        ];
     }
-
+    return true;
 }
+function show_prime($a, $b){
+    for ($i = $a; $i <= $b; $i++){
+        if(check_prime($i))
+            echo $i, '<br>';
+    }
+}
+//function isPrimeNumber(){
+//    // so nguyen n < 2 khong phai la so nguyen to
+////    if ($n < 2) {
+////        return false;
+////    }
+//    // check so nguyen to khi n >= 2
+//    $squareRoot = sqrt (30 );
+//    for($i = 11; $i <= $squareRoot; $i ++) {
+//        if (30 % $i == 0) {
+//            return [
+//                'i'=>''
+//            ];
+//        }
+//
+//    }
+//    return [
+//        'i'=>$i
+//    ];
+//
+//}
 //    function check(){
 //        for($i=11;$i<=30;$i++){
 //            echo $i;
